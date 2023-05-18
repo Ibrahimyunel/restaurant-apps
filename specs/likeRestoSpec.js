3ed979f4f293d2355d04e9e80503dfcf6f49aa57
Ibrahimyunel <ibrahim.cbshp@gmail.com>
2023-05-18 14:54:34 +0700

    create integration test of unlike a resto

--- specs/likeRestoSpec.js
+++ specs/likeRestoSpec.js
@@ -10,45 +10,37 @@ describe('Liking A Resto', () => {
 
   it('should show the like button when the Resto has not been liked before', async () => {
     await TestFactories.createLikeButtonHandlerWithResto({ id: 1 });
-
-    expect(document.querySelector('[aria-label="add to favorite button"]'))
-      .toBeTruthy();
+    const likeRestoBtn = document.querySelector('[aria-label="add to favorite button"]');
+    expect(likeRestoBtn).toBeTruthy();
   });
 
   it('should not show the unlike button when the Resto has not been liked before', async () => {
     await TestFactories.createLikeButtonHandlerWithResto({ id: 1 });
-
-    expect(document.querySelector('[aria-label="delete from favorite button"]')).toBeFalsy();
+    const unlikeRestoBtn = document.querySelector('[aria-label="delete from favorite button"]');
+    expect(unlikeRestoBtn).toBeFalsy();
   });
 
   it('should be able to like the Resto', async () => {
     await TestFactories.createLikeButtonHandlerWithResto({ id: 1 });
-
     document.querySelector('#likeButton').dispatchEvent(new Event('click'));
     const Resto = await FavoriteRestoIdb.getResto(1);
-
     expect(Resto).toEqual({ id: 1 });
-
     FavoriteRestoIdb.deleteResto(1);
   });
 
   it('should not add a Resto again when its already liked', async () => {
     await TestFactories.createLikeButtonHandlerWithResto({ id: 1 });
-
     await FavoriteRestoIdb.putResto({ id: 1 });
     // Simulasikan pengguna menekan tombol suka film
     document.querySelector('#likeButton').dispatchEvent(new Event('click'));
     // tidak ada film yang ganda
     expect(await FavoriteRestoIdb.getAllRestos()).toEqual([{ id: 1 }]);
-
     FavoriteRestoIdb.deleteResto(1);
   });
 
   it('should not add a Resto when it has no id', async () => {
     await TestFactories.createLikeButtonHandlerWithResto({});
-
     document.querySelector('#likeButton').dispatchEvent(new Event('click'));
-
     expect(await FavoriteRestoIdb.getAllRestos()).toEqual([]);
   });
 });
