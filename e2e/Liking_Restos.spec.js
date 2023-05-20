75d91af34c42ecd0823f0237b19ff95e0e3cddc3
Ibrahimyunel <ibrahim.cbshp@gmail.com>
2023-05-20 14:15:06 +0700

    create e2e testing using codecept

---
+++ e2e/Liking_Restos.spec.js
@@ -0,0 +1,38 @@
+const assert = require('assert');
+
+Feature('Liking Restos');
+
+Before(({ I }) => {
+  I.amOnPage('/');
+});
+
+Scenario('like and unlike one resto', async ({ I }) => {
+  I.seeElement('.caption h3');
+
+  const firstRestoTitle = locate('.caption h3').first();
+  const getFirstRestoTitle = await I.grabTextFrom(firstRestoTitle);
+
+  const detailRestoBtn = locate('.btn-wrapper a').first();
+  I.click(detailRestoBtn);
+
+  const likeRestoBtn = '[aria-label="add to favorite button"]';
+  I.waitForElement(likeRestoBtn, 2);
+  I.seeElement(likeRestoBtn);
+  I.click(likeRestoBtn);
+
+  I.amOnPage('/#/favorite');
+  const getRestoTitleInDetail = await I.grabTextFrom('.caption h3');
+  assert.strictEqual(getFirstRestoTitle, getRestoTitleInDetail);
+
+  // Unlike Resto Process
+  I.seeElement(detailRestoBtn);
+  I.click(detailRestoBtn);
+
+  const unlikeRestoBtn = '[aria-label="delete from favorite button"]';
+  I.waitForElement(unlikeRestoBtn, 2);
+  I.seeElement(unlikeRestoBtn);
+  I.click(unlikeRestoBtn);
+
+  I.amOnPage('/#/favorite');
+  I.dontSeeElement('Detail', '.btn-wrapper a.btn');
+});
